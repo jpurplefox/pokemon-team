b2bd2ff4047ad5b5b88c29f7c661642247efbda6
jcollado <collado.juanmaria@gmail.com>
2019-04-30 11:08:32 -0300

    Initial commit. Create Vue project

---
+++ front/src/App.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  #app
+    img(src="./assets/logo.png")
+    pk-pokemon-team
+</template>
+
+<script>
+import PkPokemonTeam from './components/PokemonTeam.vue'
+
+export default {
+  name: 'app',
+  components: { PkPokemonTeam },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ front/src/components/PokemonTeam.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  div
+    h2 Este es un gran equipo
+    ul
+      li(v-for="p in pokemons") {{ p.especie }}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      pokemons: [{ especie: 'Bulbasaur' }, { especie: 'Beedrill' }]
+    }
+  }
+}
+</script>
