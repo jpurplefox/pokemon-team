59a5c3b8c53d62e802e30ee3df7ae9aaa032482a
Jésica Collado <collado.jesica@gmail.com>
2019-11-21 14:43:03 -0300

    Clean vue initial files

--- front/tests/unit/views/PokemonTeams.spec.js
+++ front/tests/unit/views/PokemonTeams.spec.js
@@ -1,7 +1,7 @@
 import { assert, expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import sinon from '../../../node_modules/sinon/pkg/sinon-esm.js'
-import PokemonTeams from '@/components/PokemonTeams.vue'
+import PokemonTeams from '@/views/PokemonTeams.vue'
 import teamService from '@/services/teams'
 
 describe('PokemonTeams.vue', () => {
