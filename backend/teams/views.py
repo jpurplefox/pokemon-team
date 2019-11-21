af2ae32026bf6a717a32cdbbd12d7a775be8192c
Jésica Collado <collado.jesica@gmail.com>
2019-11-21 12:11:41 -0300

    Refactor: TeamDetail use RetrieveAPIView

--- backend/teams/views.py
+++ backend/teams/views.py
@@ -1,7 +1,4 @@
-from rest_framework.exceptions import NotFound
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 
 from teams import models, serializers
 
@@ -11,11 +8,6 @@ class TeamList(ListCreateAPIView):
     queryset = models.Team.objects.all()
 
 
-class TeamDetail(APIView):
-    def get(self, request, pk, format=None):
-        try:
-            team = models.Team.objects.get(pk=pk)
-        except models.Team.DoesNotExist:
-            raise NotFound()
-        serializer = serializers.TeamSerializer(team)
-        return Response(serializer.data)
+class TeamDetail(RetrieveAPIView):
+    serializer_class = serializers.TeamSerializer
+    queryset = models.Team.objects.all()
