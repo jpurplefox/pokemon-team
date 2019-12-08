5d1f3ac7c7730614d6b60b4c9620610fcc3e819f
Jésica Collado <collado.jesica@gmail.com>
2019-12-07 22:48:32 -0300

    Pokemon list endpoint return the list of pokemon

--- backend/poke_api/tests.py
+++ backend/poke_api/tests.py
@@ -29,13 +29,31 @@ class PokemonListViewTest(TestCase):
         self.factory = APIRequestFactory()
         self.view = views.PokemonList.as_view()
 
+    def perform_get(self):
+        request = self.factory.get('/api/pokemon/')
+        response = self.view(request)
+        return response
+
     def test_url_resolve_to_correct_view(self):
         match = resolve('/api/pokemon/')
 
         self.assertEquals(match.func.view_class, views.PokemonList)
 
-    def test_get_pokemon_list_return_http_200(self):
-        request = self.factory.get('/api/pokemon/')
-        response = self.view(request)
+    @patch('poke_api.views.PokeApi')
+    def test_get_pokemon_list_return_http_200(self, poke_api_mock):
+        response = self.perform_get()
 
         self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    @patch('poke_api.views.PokeApi')
+    def test_get_pokemon_list_return_the_list(self, poke_api_mock):
+        pokemon_list = [
+            {'name': 'bulbasaur', 'url': 'https://pokeapi.co/api/v2/pokemon/1/'},
+            {'name': 'ivysaur', 'url': 'https://pokeapi.co/api/v2/pokemon/2/'},
+        ]
+
+        poke_api_mock.return_value.get_all_pokemon.return_value = pokemon_list
+        response = self.perform_get()
+
+        poke_api_mock.return_value.get_all_pokemon.assert_called_once()
+        self.assertEqual(response.data, pokemon_list)
--- backend/poke_api/views.py
+++ backend/poke_api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from poke_api.services import PokeApi
+
 class PokemonList(APIView):
     def get(self, request, format=None):
-        return Response()
+        pokemon_list = PokeApi().get_all_pokemon()
+        return Response(pokemon_list)
